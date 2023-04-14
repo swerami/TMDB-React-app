b38d984d022ca7d838950d50b598800bb50bb285
TechRami <techrami@outlook.com>
2023-04-14 03:30:04 +0300

    lifted the active state up to Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,6 +9,8 @@ import { BsPeople } from "react-icons/bs";
 import { GoFileMedia } from "react-icons/go";
 import { IoSettingsOutline } from "react-icons/io5";
 import { IconType } from "react-icons/lib";
+import { CiLogout } from "react-icons/ci";
+import { useState } from "react";
 
 const listItems: { [key: string]: IconType } = {
   Home: AiOutlineHome,
@@ -17,9 +19,20 @@ const listItems: { [key: string]: IconType } = {
   Coming_soon: AiOutlineFieldTime,
 };
 
+const socialItems: { [key: string]: IconType } = {
+  Friends: BsPeople,
+  Media: GoFileMedia,
+};
+
+const generalItems: { [key: string]: IconType } = {
+  settings: IoSettingsOutline,
+  logout: CiLogout,
+};
+
 const Navbar = () => {
+  const [active, setActive] = useState("");
   return (
-    <nav className="border-r h-full">
+    <nav className="border-r h-screen">
       <div className="flex flex-row items-center gap-2">
         <img
           className="w-12 text-red-500"
@@ -28,7 +41,24 @@ const Navbar = () => {
         />
         <p className="font-bold text-2xl">Exxmon.</p>
       </div>
-      <SidebarList label={"menu"} list={listItems} />
+      <SidebarList
+        active={active}
+        onSelect={(key) => setActive(key)}
+        label={"menu"}
+        list={listItems}
+      />
+      <SidebarList
+        active={active}
+        onSelect={(key) => setActive(key)}
+        label={"Social"}
+        list={socialItems}
+      />
+      <SidebarList
+        active={active}
+        onSelect={(key) => setActive(key)}
+        label={"general"}
+        list={generalItems}
+      />
     </nav>
   );
 };
--- src/components/SidebarList.tsx
+++ src/components/SidebarList.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
 import { IconType } from "react-icons/lib";
 
 interface Props {
   label: string;
   list: { [key: string]: IconType };
+  onSelect: (key: string) => void;
+  active: string;
 }
 
-const SidebarList = ({ list, label }: Props) => {
-  const [activeItem, setActiveItem] = useState<string>("Home");
+const SidebarList = ({ list, label, onSelect, active }: Props) => {
   return (
     <ul className="flex flex-col gap-8 py-8">
       <label className="text-gray-400 capitalize">{label}</label>
@@ -18,19 +18,19 @@ const SidebarList = ({ list, label }: Props) => {
           <li
             key={key}
             className={`flex items-center gap-2 cursor-pointer
-            ${activeItem === key ? "border-r-4 border-red-500" : ""}
+            ${active === key ? "border-r-4 border-red-500" : ""}
             `}
-            onClick={() => setActiveItem(key)}
+            onClick={() => onSelect(key)}
           >
             <Icon
               className={`w-5 h-5 transition-colors duration-300 ${
-                activeItem === key ? "text-red-500" : "text-gray-400"
+                active === key ? "text-red-500" : "text-gray-400"
               }`}
             />
             <span
               className={`
               transition-colors duration-300
-            ${activeItem === key ? "text-gray-500 font-bold" : "text-gray-400"}
+            ${active === key ? "text-gray-500 font-bold" : "text-gray-400"}
             `}
             >
               {formattedKey}
